f45614826880a0a7a3ccd8f7023eb0b11c8e30c9
Charidan <charidan@github.com>
2015-04-11 19:04:54 -0700

    Cloned the debug utility from another project

---
+++ asg1/debug.cpp
@@ -0,0 +1,37 @@
+// $Id: rlnichol debug.cpp,v 1.1 2015-01-10 11:35:40-08 - - $
+//      edhyang
+
+#include <climits>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+#include "debug.h"
+#include "util.h"
+
+debugflags::flagset debugflags::flags {};
+
+void debugflags::setflags (const string& initflags) {
+   for (const unsigned char flag: initflags) {
+      if (flag == '@') flags.set();
+                  else flags.set (flag, true);
+   }
+}
+
+//
+// getflag -
+//    Check to see if a certain flag is on.
+//
+
+bool debugflags::getflag (char flag) {
+   // WARNING: Don't TRACE this function or the stack will blow up.
+   return flags.test (static_cast<unsigned char> (flag));
+}
+
+void debugflags::where (char flag, const char* file, int line,
+                        const char* func) {
+   cout << execname() << ": DEBUG(" << flag << ") "
+        << file << "[" << line << "] " << func << "()" << endl;
+}
+
