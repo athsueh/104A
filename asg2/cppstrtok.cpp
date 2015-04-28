d5b061c3eedaba36d77322432edd8d9307b4215b
Charidan <charidan@github.com>
2015-04-27 23:40:17 -0700

    Removed compiler warnings

--- asg2/cppstrtok.cpp
+++ asg2/cppstrtok.cpp
@@ -1,4 +1,4 @@
-// $Id: cppstrtok.cpp,v 1.2 2015-04-26 20:23:46-07 - - $
+// $Id: cppstrtok.cpp,v 1.1 2015-04-27 20:37:06-07 - - $
 
 // Use cpp to scan a file and print line numbers.
 // Print out each input line read in, then strtok it for
@@ -31,40 +31,6 @@ void chomp (char* string, char delim) {
    if (len == 0) return;
    char* nlpos = string + len - 1;
    if (*nlpos == delim) *nlpos = '\0';
-}
-
-
-// Run cpp against the lines of the file.
-void cpplines (FILE* pipe, char* filename) {
-   int linenr = 1;
-   char inputname[LINESIZE];
-   strcpy (inputname, filename);
-   for (;;) {
-      char buffer[LINESIZE];
-      //char* fgets_rc = fgets (buffer, LINESIZE, pipe);
-      //if (fgets_rc == NULL) break;
-      chomp (buffer, '\n');
-      DEBUGF ('T', "%s:line %d: [%s]\n", filename, linenr, buffer);
-      // http://gcc.gnu.org/onlinedocs/cpp/Preprocessor-Output.html
-      int sscanf_rc = sscanf (buffer, "# %d \"%[^\"]\"",
-                              &linenr, filename);
-      if (sscanf_rc == 2) {
-         DEBUGF ('T', "DIRECTIVE: line %d file \"%s\"\n",
-                 linenr, filename);
-         continue;
-      }
-      char* savepos = NULL;
-      char* bufptr = buffer;
-      for (int tokenct = 1;; ++tokenct) {
-         char* token = strtok_r (bufptr, " \t\n", &savepos);
-         bufptr = NULL;
-         if (token == NULL) break;
-         DEBUGF ('T', "token %d.%d: [%s]\n",
-                 linenr, tokenct, token);
-         intern_stringset(token);
-      }
-      ++linenr;
-   }
 }
 
 string scan_options (int argc, char** argv) {
